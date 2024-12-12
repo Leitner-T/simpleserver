2e3e688a607d5cf353d2d318c8c43065066246fa
Thomas Leitner <leitne200015@sr.htlweiz.at>
2024-12-12 14:42:33 +0100

    12.12.2024

--- simpleserver/simple_server.py
+++ simpleserver/simple_server.py
@@ -9,21 +9,34 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         self.request.sendall(
-            b"Hi there: " + self.client_address[0].encode("utf-8") + b"\n"
+            b"Hi there on the server from thomas: " + self.client_address[0].encode("utf-8") + b"\n"
         )
         self.request.sendall(b"Type 'quit' to quit.\n")
         while True:
             self.data = self.request.recv(1024).strip()
             logger.debug("Received from %s: %s", self.client_address[0], self.data)
-            self.request.sendall(
-                self.data + b" is in upper case: " + self.data.upper() + b"\n"
+
+            # Zählen der Anzahl der 'o's in der Anfrage
+            count_o = self.data.decode('utf-8').lower().count('o')
+
+            # Eingang in umgekehrter Reihenfolge
+            reversed_data = self.data.decode('utf-8')[::-1].encode('utf-8')
+            
+            # Antwort, die die Anzahl der 'o's und die umgekehrte Eingabe enthält
+            response = (
+                b"thomas server sagt: " + self.data + 
+                b" is in upper case: " + self.data.upper() +
+                b"\nReversed input: " + reversed_data +
+                b"\nNumber of 'o's: " + str(count_o).encode("utf-8") + b"\n"
             )
+            self.request.sendall(response)
+
             if self.data.upper() == b"QUIT":
                 break
         logger.debug("Client %s left", self.client_address[0])
 
 
-class MyTCPServer(socketserver.TCPServer):
+class MyTCPServer(socketserver.ThreadingTCPServer):
 
     HOST, PORT = "0.0.0.0", 9999
 
